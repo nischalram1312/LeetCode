075bc1ceebe8a6686613dded29fa8fde5eb5638f
Nischal Raghuram Puvvala <90822991+nischalram1312@users.noreply.github.com>
2022-07-17 01:00:58 +0530

    Time: 2 ms (11.27%), Space: 42.5 MB (46.27%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,18 +1,18 @@
 class Solution {
     public void sortColors(int[] nums) {
-        boolean swapped = false;
+        // boolean swapped = false;
         for(int i=0; i<nums.length; i++){
             for(int j=1; j<nums.length-i; j++){
                 if(nums[j]<nums[j-1]){
                     int temp = nums[j];
                     nums[j] = nums[j-1];
                     nums[j-1] = temp;
-                    swapped = true;
+                    // swapped = true;
                 }
             }
-            if(swapped == false){
-                break;
-            }
+            // if(swapped == false){
+            //     break;
+            // }
         }
         // System.out.print(nums);
     }
