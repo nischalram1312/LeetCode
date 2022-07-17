a387d9d5a6d761b881f13bd433c59ac77b1686cd
Nischal Raghuram Puvvala <90822991+nischalram1312@users.noreply.github.com>
2022-07-18 04:41:50 +0530

    Time: 925 ms (7.32%), Space: 64 MB (50.83%) - LeetHub

--- 888-fair-candy-swap/888-fair-candy-swap.java
+++ 888-fair-candy-swap/888-fair-candy-swap.java
@@ -8,15 +8,20 @@ class Solution {
         for (int i = 0; i < bobSizes.length; i++){
             sumBob+=bobSizes[i];
         }
-        int x = (sumAlice-sumBob)/2;
-        System.out.print(sumAlice+" "+sumBob+" "+x);
+        int diff = (sumAlice-sumBob)/2;
+        //alice gives x candies to bob
+        //bob givevs y candies to alice
+        //sumAlice-x+y=sumBob+x-y
+        //equation will be y = (sumAlice-sumBob)/2 + x;
+        System.out.print(sumAlice+" "+sumBob+" "+diff);
         
         
         int[] arr = new int[2];
         
         for(int i=0; i<aliceSizes.length; i++){
             for(int j=0; j<bobSizes.length; j++){
-                if(aliceSizes[i]==bobSizes[j]+x){
+                //equation above
+                if(aliceSizes[i]==diff+bobSizes[j]){
                     arr[0]=aliceSizes[i];
                     arr[1]=bobSizes[j];
                 }
