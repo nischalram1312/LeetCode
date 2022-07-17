cd4d42253dfa1411708dd84cc95dba9986669e95
Nischal Raghuram Puvvala <90822991+nischalram1312@users.noreply.github.com>
2022-07-18 03:57:16 +0530

    Time: 12 ms (30.71%), Space: 51.1 MB (33.06%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -2,14 +2,27 @@ class Solution {
     public int singleNumber(int[] nums) {
         Arrays.sort(nums);
         
-        for(int i=0; i<nums.length-1; i++){
-            if(i%2==0){
-                if(nums[i]!=nums[i+1]){
-                    return nums[i];
-                }
+//         for(int i=0; i<nums.length-1; i++){
+//             if(i%2==0){
+//                 if(nums[i]!=nums[i+1]){
+//                     return nums[i];
+//                 }
                 
+//             }
+//         }
+        int count =1;
+        for(int i=0; i<nums.length-1; i++){
+            if(nums[i]==nums[i+1]){
+                count++;                
+            }
+            else if(count ==1){
+                return nums[i];
+            }
+            else{
+                count =1;
             }
         }
+        
         return nums[nums.length-1];
     }
 }
