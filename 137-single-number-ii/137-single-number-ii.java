e5985b6ec76ace200dc15de885f16a44a13e0243
Nischal Raghuram Puvvala <90822991+nischalram1312@users.noreply.github.com>
2022-07-18 03:54:33 +0530

    Time: 5 ms (60.47%), Space: 44.5 MB (36.15%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int count =1;
+        for(int i=0; i<nums.length-1; i++){
+            if(nums[i]==nums[i+1]){
+                count++;
+            }
+            else if(count==1){
+                return nums[i];
+            }
+            else{
+                count =1;
+            }
+        }
+        return nums[nums.length-1];
+    }
+}
